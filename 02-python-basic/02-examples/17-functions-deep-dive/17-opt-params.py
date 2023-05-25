622a5da2b9eb8e00d0dd59f3d72c7697cacead5d
dpinezich <david.pinezich@gmail.com>
2023-05-25 07:49:27 +0200

    added func

---
+++ 02-python-basic/02-examples/17-functions-deep-dive/17-opt-params.py
@@ -0,0 +1,9 @@
+def volume(width, length, depth=1, color="black"):
+    print("Values:", width, length, depth, color)
+    print("Volume:", width * length * depth, "Farbe:", color)
+
+
+volume(4, 6, 2, "red")
+volume(2, 12, 7)
+volume(5, 8)
+volume(4, 7, color="red")
