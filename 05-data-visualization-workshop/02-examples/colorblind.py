447b3e7d56989fd4eb5ad9b7a61eae77a6f5c8a4
dpinezich <david.pinezich@gmail.com>
2023-09-18 00:38:59 +0200

    updated

--- 05-data-visualization-workshop/02-examples/colorblind.py
+++ 05-data-visualization-workshop/02-examples/colorblind.py
@@ -1,48 +1,20 @@
-import matplotlib
-from matplotlib.colors import ListedColormap
-import numpy as np
-import viscid
-from viscid.plot import vpyplot as vlt
+import seaborn as sns
 import matplotlib.pyplot as plt
 
-matplotlib.rcParams.update(matplotlib.rcParamsDefault)
+# Apply the default theme
+sns.set_theme()
 
-fld = viscid.empty((np.linspace(1, 5, 64), np.linspace(1, 5, 64)),
-                   name="F", pretty_name="Generic Field")
-X, Y = fld.get_crds(shaped=True)
-fld[:, :] = 1e-4 * (np.sin(X)**10 + np.cos(10 + X * Y) * np.cos(X))
+# Load an example dataset
+tips = sns.load_dataset("tips")
 
-with plt.style.context(("seaborn-colorblind",)):
-    fig = plt.figure(figsize=(11, 7))
+# Set the palette to the "pastel" default palette:
+sns.set_palette("colorblind") #try also deep, muted, pastel, bright, dark
 
-    ax = plt.subplot2grid((2, 9), (0, 0), rowspan=2)
-    pal = vlt.get_current_colorcycle()
-    size = 1
-    n = len(pal)
-    ax.imshow(np.arange(n).reshape(n, 1), cmap=ListedColormap(list(pal)),
-              interpolation="nearest", aspect="auto")
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-    ax.set_ylabel("Color Cycle")
+# Create a visualization
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
 
-    plt.subplot2grid((2, 9), (0, 1), rowspan=2, colspan=4)
-    x = np.linspace(0, 2 * np.pi)
-    for phase in np.linspace(0, np.pi / 4, n):
-        plt.plot(x, (1 + np.sqrt(phase)) * np.sin(x - phase),
-                     label=r"$\phi = {0:.2g}$".format(phase))
-    plt.legend(loc=0)
-
-    plt.subplot2grid((2, 9), (0, 5), colspan=4)
-    vlt.plot(fld)
-    plt.title("Sequential")
-
-    plt.subplot2grid((2, 9), (1, 5), colspan=4)
-    vlt.plot(fld, lin=0)
-    plt.title("Symmetric")
-
-    vlt.auto_adjust_subplots(subplot_params=dict(top=0.93, bottom=0.1))
-    txt = ("Matplotlib Version: {0}\nViscid Version: {1}"
-           "".format(matplotlib.__version__, viscid.__version__))
-    fig.text(0.05, 0.01, txt, color='grey', size='small')
+plt.show()
